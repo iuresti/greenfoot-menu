c620d2ab2954872e76fdb98433ed20da92688c0b
Ivan Uresti <ivan.uresti@gmail.com>
2022-05-16 08:35:36 -0500

    attend PR comments

--- ElephantButton.java
+++ ElephantButton.java
@@ -1,17 +1,7 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import greenfoot.*; 
 
-/**
- * Write a description of class ElephantButton here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class ElephantButton extends Actor
 {
-    /**
-     * Act - do whatever the ElephantButton wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
     public void act()
     {
         GreenfootImage image = getImage();
